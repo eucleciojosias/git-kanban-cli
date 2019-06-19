0f2fb73ba7f07058ac1e2d287ddf774341a31dda
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2019-06-19 11:44:55 -0300

    Adjust dashboard to get data for 4 weeks
    Fix chart titles and data

--- python/dashboard/src/components/Throughput/index.js
+++ python/dashboard/src/components/Throughput/index.js
@@ -3,27 +3,23 @@ import {
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 
-class LastWeek extends Component {
-
-  toPercent = (decimal, fixed = 0) => (
-    `${((decimal / this.props.total) * 100).toFixed(fixed)}%`
-  )
+class Throughput extends Component {
 
   render() {
     const { data } = this.props;
     return (
       <BarChart
         ref={reference => this.chart = reference}
-        width={500}
-        height={300}
+        width={350}
+        height={250}
         data={data}
         margin={{
           top: 5, right: 30, left: 20, bottom: 5,
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis tickFormatter={this.toPercent} />
+        <XAxis dataKey="date" />
+        <YAxis />
         <Tooltip />
         <Bar dataKey="total" label fill="#8884d8" barSize={30}/>
       </BarChart>
@@ -32,4 +28,4 @@ class LastWeek extends Component {
 }
 
 
-export default LastWeek;
+export default Throughput;
