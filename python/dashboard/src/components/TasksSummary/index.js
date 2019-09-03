c315265b336f0223f72d5a04c1ce6234ab6aeadd
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2019-09-03 14:44:30 -0300

    Get data from remote files to dashboard

--- python/dashboard/src/components/TasksSummary/index.js
+++ python/dashboard/src/components/TasksSummary/index.js
@@ -1,34 +1,22 @@
 import React, { Component, Fragment } from 'react';
-import { Icon } from 'semantic-ui-react';
 import { Wrapper, Container, Divider } from './styles';
 
-class stat extends Component {
-  state = {
-    data: [
-      { stage: 'To Do', icon: 'clipboard list', total: 248 },
-      { stage: 'In Progress', icon: 'sync alternate', total: 7 },
-      { stage: 'Review', icon: 'redo alternate', total: 12 },
-      { stage: 'Testing', icon: 'file alternate outline', total: 4 },
-      { stage: 'Late', icon: 'clock outline', total: 0 },
-      { stage: 'Done', icon: 'check', total: 2304 },
-    ],
-  }
+class TasksSummary extends Component {
 
   render() {
-    const { data } = this.state;
+    const { data } = this.props;
     return (
       <Wrapper>
-        {data.map(stat => (
-          <Fragment>
-            <Container key={stat.stage}>
+        {data.map((item, i) => (
+          <Fragment key={ i }>
+            <Container>
               <div>
-                <Icon name={stat.icon} size="large" color="#747B84" />
                 <h4>
-                  {stat.stage}
+                  { item.stage }
                 </h4>
               </div>
               <div>
-                <span>{ stat.total }</span>
+                <span>{ item.total }</span>
               </div>
             </Container>
             <Divider />
@@ -39,4 +27,4 @@ class stat extends Component {
   }
 }
 
-export default stat;
+export default TasksSummary;
